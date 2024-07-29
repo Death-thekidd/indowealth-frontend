d329c39c07cc9c0232bf7db6da7a83e03cdb085e
Divine Attah-Ohiemi <ohiemidivine7@gmail.com>
2024-07-29 15:41:08 +0100

    update

--- dist/indowealth-frontend/server/main.server.mjs
+++ dist/indowealth-frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-WK7R5GXL.mjs";import"./chunk-N4EC2U4M.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-BFM6H6KR.mjs";import"./chunk-N4EC2U4M.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
